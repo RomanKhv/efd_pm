e0c55525820394df43b2b1a2d9a1d63ac25f8178
Roman Khvostikov <roman_khvostikov@mentor.com>
2022-03-23 23:59:33 +0300

    Very basic PM-style controls generation with react-bootstrap

--- src/Feature.js
+++ src/Feature.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Accordion, Form, InputGroup } from "react-bootstrap";
 
 class FeatureView extends Component {
     constructor(props) {
@@ -14,19 +15,72 @@ class FeatureView extends Component {
     }
 
     render() {
+        // return (
+        //     <Form>
+        //         <Accordion defaultActiveKey={['0']} alwaysOpen>
+        //             <Accordion.Item eventKey="0">
+        //                 <Accordion.Header>Contours</Accordion.Header>
+        //                 <Accordion.Body>
+        //                     <InputGroup>
+        //                         <InputGroup.Text>Parameter</InputGroup.Text>
+        //                         <Form.Select>
+        //                             <option>Pressure</option>
+        //                             <option>Temperature</option>
+        //                         </Form.Select>
+        //                     </InputGroup>
+        //                     <InputGroup>
+        //                         <InputGroup.Text>Max</InputGroup.Text>
+        //                         <Form.Control type="number" value="101325.2"/>
+        //                     </InputGroup>
+        //                 </Accordion.Body>
+        //             </Accordion.Item>
+        //             <Accordion.Item eventKey="1">
+        //                 <Accordion.Header>Options</Accordion.Header>
+        //                 <Accordion.Body>
+        //                     Transparency is 1
+        //                 </Accordion.Body>
+        //             </Accordion.Item>
+        //         </Accordion>
+        //     </Form>
+        // )
         let f = this.state.feature;
         let prop_arr = [];
         for (let key in f) {
             if (key === 'name')
-                continue;
-            let sview = key + ': ' + String(f[key]);
-            prop_arr.push(<p>{sview}</p>);
+            continue;
+            const val = f[key];
+            // let sview = key + ': ' + String(f[key]);
+            let ctrl = {};
+            switch (typeof(val)) {
+                case 'string':
+                    ctrl = <Form.Select>
+                        <option>Pressure</option>
+                        <option>Temperature</option>
+                    </Form.Select>;
+                    break;
+                case 'number':
+                    ctrl = <Form.Control type="number" value={val}/>;
+                    break;
+            }
+            prop_arr.push(
+                <InputGroup>
+                    <InputGroup.Text>{key}</InputGroup.Text>
+                    {ctrl}
+                </InputGroup>
+            );
         }
         return (
             <div>
-                {prop_arr}
+                <Accordion defaultActiveKey={['0']} alwaysOpen>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Contours</Accordion.Header>
+                        <Accordion.Body>
+                            {prop_arr}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
             </div>
-        )
+        );
     }
 }
 
